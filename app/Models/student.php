bf04f92161bc9591225adc529adf09b45f0b8230
fsilva <fsilva@urbano.com.pe>
2025-01-08 15:34:30 -0500

    commit of portafolio

--- app/Models/student.php
+++ app/Models/student.php
@@ -5,15 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class student extends Model
+class Student extends Model
 {
     use HasFactory;
     protected $table ='students';
     protected $primaryKey = 'id';
     protected $fillable = [
-        'first_name', 
-        'last_name', 
-        'email', 
+        'first_name',
+        'last_name',
+        'email',
         'phone',
         'birth_date',
         'enrollment_date',
